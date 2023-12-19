6c0c2922117fe974fa78f4920bea2111ca6d4479
dungdtevn <154307146+dungdtevn@users.noreply.github.com>
2023-12-19 13:59:04 +0700

    update program id

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -12,7 +12,7 @@ use mpl_token_metadata::{
     state::DataV2,
 };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("D7Um7GCqcvKHoucousuNur5zX3HFGHmZVuDfsoke2nrv");
 #[program]
 pub mod solana_nft_anchor {
 
